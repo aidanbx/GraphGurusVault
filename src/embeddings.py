9953d17eee98540d5c9aafc5db835c72395da462
Kenny Lau <klau24@calpoly.edu>
2023-02-09 11:41:36 -0800

    added vault dataset and creating embeddings. still need to preprocess md files but works as of now

--- src/embeddings.py
+++ src/embeddings.py
@@ -1,21 +1,40 @@
 import os
 import openai
+from tenacity import (
+    retry,
+    stop_after_attempt,
+    wait_random_exponential,
+)  # for exponential backoff
 
 openai.api_key = os.environ["OPENAI_API_KEY"]
 
-def get_embedding(block):
+# tenacity helps with rate limits
+# checkout here: https://github.com/openai/openai-cookbook/blob/main/examples/How_to_handle_rate_limits.ipynb
+@retry(wait=wait_random_exponential(min=5, max=60), stop=stop_after_attempt(10))
+def createEmbedding(block):
     # returns array of size 1536
     return openai.Embedding.create(input = [block], model="text-embedding-ada-002")['data'][0]['embedding']
 
-def extract_mds(path):
-    # walk through dir to get all markdown files
-    # returns a dictionary where {filename1: content1, filename2: content2, ...}
+def preprocessText(text):
     pass
 
+def getAllEmbeddings(path):
+    # recursively walk through dir to get all markdown files
+    # returns a dictionary where {filename1: content1, filename2: content2, ...}
+    embeddings = {}
+    for item in os.listdir(path)[:2]:
+        print(item)
+        content = os.path.join(path, item)
+        if os.path.isdir(content):
+            embeddings.update(getAllEmbeddings(content))
+        elif content.endswith(".md"):
+            fp = open(content)
+            embeddings[item] = createEmbedding(fp.read())
+            fp.close()
+    return embeddings
 
 
 
-# Define the text to be embedded
-text = "This is a sample text."
-word_embeddings = get_embedding(text)
-print(len(word_embeddings))
+embeddings = getAllEmbeddings("./my-second-brain")
+print(embeddings)
+print(embeddings.keys())
